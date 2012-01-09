7971cadcf54f1ecc7135c201d6cb9b3424582dc2
Cam Pedersen <diffference@gmail.com>
2012-01-09 10:52:08 -0500

    [major] working get

--- scp.js
+++ scp.js
@@ -4,14 +4,31 @@
  */
 var exec = require('child_process').exec;
 
-var scp = {};
+var scp = module.exports = {};
 
+/*
+ * Transfer a file to a remote host
+ */
 scp.send = function (options, cb) {
-
+  exec ('scp ', function () {
+    cb(arguments);
+  });
 }
 
+/*
+ * Grab a file from a remote host
+ */
 scp.get = function (options, cb) {
-
+  var command = [
+    'scp',
+    options.host + ':' + options.file,
+    options.path
+  ];
+  exec(command.join(' '), function (err, stdout, stderr) {
+    if (cb) {
+      cb(err, stdout, stderr);
+    } else {
+      if (err) throw new Error(err);
+    }
+  });
 }
-
-module.exports = scp;
