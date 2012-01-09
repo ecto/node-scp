d20270c10f9313ceeca9e0b18e8273c8466deb84
Cam Pedersen <diffference@gmail.com>
2012-01-09 10:34:42 -0500

    [minor] stub out module functions

---
+++ scp.js
@@ -0,0 +1,17 @@
+/*
+ * node-scp
+ * <cam@onswipe.com>
+ */
+var exec = require('child_process').exec;
+
+var scp = {};
+
+scp.send = function (options, cb) {
+
+}
+
+scp.get = function (options, cb) {
+
+}
+
+module.exports = scp;
