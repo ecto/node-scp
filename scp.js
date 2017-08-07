25846f616fc872093e16d990e2a1271fe2a12d88
tbouchnafa <thami.bouchnafa@1und1.de>
2017-08-07 12:41:30 +0200

    Add additional ssh options

--- scp.js
+++ scp.js
@@ -6,6 +6,15 @@ var exec = require('child_process').exec;
 
 var scp = module.exports = {};
 
+function buildSshOptions(options) {
+    result = '';
+    var ssh_options = options.ssh_options || {}
+    ssh_options['ControlMaster'] = 'no'
+    Object.keys(ssh_options).map(function (key) {
+        result += "-o " + key + '=' + ssh_options[key] + ' ' 
+    })
+    return result.trim();
+}
 /*
  * Transfer a file to a remote host
  */
@@ -15,7 +24,7 @@ scp.send = function (options, cb) {
     '-r',
     '-P',
     (options.port == undefined ? '22' : options.port),
-    '-o "ControlMaster no"', //callback is not fired if ssh sessions are shared
+    buildSshOptions(options),
     options.file,
     (options.user == undefined ? '' : options.user+'@') + options.host + ':' + options.path,
   ];
@@ -32,12 +41,14 @@ scp.send = function (options, cb) {
  * Grab a file from a remote host
  */
 scp.get = function (options, cb) {
+  var ssh_options = ["ControlMaster=no"]; //callback is not fired if ssh sessions are shared
+  ssh_options = options.ssh ? ssh_options.concat(options.ssh) : ssh_options;
   var command = [
     'scp',
     '-r',
     '-P',
     (options.port == undefined ? '22' : options.port),
-    '-o "ControlMaster no"', //callback is not fired if ssh sessions are shared
+    buildSshOptions(options),
     (options.user == undefined ? '' : options.user+'@') + options.host + ':' + options.file,
     options.path
   ];
